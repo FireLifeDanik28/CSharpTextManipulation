d27c9fdaff4451a7ffe6892a883dad2fed697350
FireLifeDanik28 <lifedanik2006@gmail.com>
2025-11-25 11:47:08 +0100

    Add project files.

---
+++ CSharpTextManipulation/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharpTextManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zadanie 1
+            //Zamiana wielkości liter
+            Console.WriteLine("Proszę wpisać zdanie");
+            string zad1 = Console.ReadLine();
+            Console.WriteLine(zad1.Length);
+            Console.WriteLine(zad1.ToUpper());
+            Console.WriteLine(zad1.ToLower());
+            
+
+        }
+    }
+}
