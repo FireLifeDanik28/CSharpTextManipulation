3a5da56e71b428072276e1d6a24d6a0ed4dda6d8
FireLifeDanik28 <lifedanik2006@gmail.com>
2025-11-25 12:12:00 +0100

    zad 1.2

--- CSharpTextManipulation/Program.cs
+++ CSharpTextManipulation/Program.cs
@@ -7,11 +7,33 @@
             //zadanie 1
             //Zamiana wielkości liter
             Console.WriteLine("Proszę wpisać zdanie");
-            string zad1 = Console.ReadLine();
+            string zad1 = Console.ReadLine().ToString();
             Console.WriteLine(zad1.Length);
-            Console.WriteLine(zad1.ToUpper());
-            Console.WriteLine(zad1.ToLower());
-            
+            Console.WriteLine("Chcesz wypisać (małymi/dużymi/tylko pierwsza duża)");
+            //zad 1.1
+            while (true)
+            {
+                string zad1choice = Console.ReadLine().ToString();
+                if (zad1choice == "małymi" || zad1choice == "malymi")
+                {
+                    Console.WriteLine(zad1.ToLower());
+                    break;
+                }
+                else if (zad1choice == "dużymi" || zad1choice == "duzymi")
+                {
+                    Console.WriteLine(zad1.ToUpper());
+                    break;
+                }
+                //zad 1.2
+                else if (zad1choice == "tylko pierwsza duża" || zad1choice == "tylko pierwsza duza")
+                {
+                    Console.WriteLine(zad1.Substring(0, 1).ToUpper() + zad1.Substring(1, zad1.Length - 1).ToLower());
+                    break;
+                }
+                else
+                    Console.WriteLine("możesz tylko napisać: małymi, malymi, dużymi, duzymi, tylko pierwsza duża, tylko pierwsza duza");
+            }
+
 
         }
     }
