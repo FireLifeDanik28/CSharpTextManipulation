f98357444fea9b509edb99d56f2a9eec6c30294d
FireLifeDanik28 <lifedanik2006@gmail.com>
2025-11-25 12:15:22 +0100

    zad1 improv

--- CSharpTextManipulation/Program.cs
+++ CSharpTextManipulation/Program.cs
@@ -9,30 +9,32 @@
             Console.WriteLine("Proszę wpisać zdanie");
             string zad1 = Console.ReadLine().ToString();
             Console.WriteLine(zad1.Length);
-            Console.WriteLine("Chcesz wypisać (małymi/dużymi/tylko pierwsza duża)");
+            Console.WriteLine("Chcesz wypisać (małymi: 1/dużymi: 2/tylko pierwsza duża: 3)");
             //zad 1.1
             while (true)
             {
                 string zad1choice = Console.ReadLine().ToString();
-                if (zad1choice == "małymi" || zad1choice == "malymi")
+                if (zad1choice == "małymi" || zad1choice == "malymi" || zad1choice == "1")
                 {
                     Console.WriteLine(zad1.ToLower());
                     break;
                 }
-                else if (zad1choice == "dużymi" || zad1choice == "duzymi")
+                else if (zad1choice == "dużymi" || zad1choice == "duzymi" || zad1choice == "2")
                 {
                     Console.WriteLine(zad1.ToUpper());
                     break;
                 }
                 //zad 1.2
-                else if (zad1choice == "tylko pierwsza duża" || zad1choice == "tylko pierwsza duza")
+                else if (zad1choice == "tylko pierwsza duża" || zad1choice == "tylko pierwsza duza" || zad1choice == "3")
                 {
                     Console.WriteLine(zad1.Substring(0, 1).ToUpper() + zad1.Substring(1, zad1.Length - 1).ToLower());
                     break;
                 }
                 else
-                    Console.WriteLine("możesz tylko napisać: małymi, malymi, dużymi, duzymi, tylko pierwsza duża, tylko pierwsza duza");
+                    Console.WriteLine("możesz tylko napisać: małymi, malymi, dużymi, duzymi, tylko pierwsza duża, tylko pierwsza duza, 1, 2, 3");
             }
+            //zad 2
+            //Długość tekstu i pierwsza/ostatnia litera
 
 
         }
